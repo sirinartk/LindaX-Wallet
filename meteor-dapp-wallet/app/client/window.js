cbb403840e0edc541a52268b21fb2480a27da4f5
David Stoneham <dstoneham.93@gmail.com>
2018-09-15 18:12:19 +1000

    UI Updates

--- meteor-dapp-wallet/app/client/window.js
+++ meteor-dapp-wallet/app/client/window.js
@@ -4,19 +4,3 @@ $(window).on('blur', function(e) {
 $(window).on('focus', function(e) {
   $('body').removeClass('app-blur');
 });
-
-// add class to the header when scrolling
-$(window).on('scroll', function() {
-  var scrollPosition = $(window).scrollTop();
-
-  if (scrollPosition > 150) {
-    $('.dapp-sticky-bar').addClass('sticky');
-    $('.dapp-header').addClass('dapp-small');
-  } else if (scrollPosition > 48) {
-    $('.dapp-header').addClass('dapp-small');
-    $('.dapp-sticky-bar').removeClass('sticky');
-  } else {
-    $('.dapp-header').removeClass('dapp-small');
-    $('.dapp-sticky-bar').removeClass('sticky');
-  }
-});
